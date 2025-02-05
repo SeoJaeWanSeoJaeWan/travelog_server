c4eaac2fc21e41c1aa9331e126f79e8bd032ad6a
SeoJaeWan <sjw7324@igis.co.kr>
2025-02-05 17:06:52 +0900

    init

---
+++ travelog_server/src/main/java/com/travel/travelog_server/TravelogServerApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelog_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelogServerApplication.class, args);
+	}
+
+}
---
+++ travelog_server/src/test/java/com/travel/travelog_server/TravelogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travelog_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
